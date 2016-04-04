d7e17556bcd6430b9ec703b7f76d37e2795bc9be
CapsAdmin <eliashogstvedt@gmail.com>
2016-04-04 15:50:10 +0200

    load system included ncurses for now

--- examples/curses/libncurses.lua
+++ examples/curses/libncurses.lua
@@ -435,7 +435,7 @@ int COLOR_PAIR(int);
 ffi.cdef("typedef uint64_t chtype;")
 ffi.cdef(header)
 
-local lib = assert(ffi.load(jit.os == "Windows" and "pdcurses" or "ncursesw"))
+local lib = assert(ffi.load(jit.os == "Windows" and "pdcurses" or "ncurses.so.5.9"))
 
 local curses = {
 	lib = lib,
