4dd8e8bd4d298f6bffb3cc6bc081704ecfa55044
CapsAdmin <eliashogstvedt@gmail.com>
2016-04-08 22:10:23 +0200

    added missing structs and enums to libsndfile

--- examples/libsndfile/build.lua
+++ examples/libsndfile/build.lua
@@ -15,9 +15,19 @@ local header = ffibuild.BuildCHeader([[
 local meta_data = ffibuild.GetMetaData(header)
 local header = meta_data:BuildMinimalHeader(function(name) return name:find("^sf_") end, function(name) return name:find("^SF") end, true, true)
 
+local extra = ""
+
+for name, struct in pairs(meta_data.structs) do
+	if name:find("^struct SF_") and not header:find(name) then
+		extra = extra .. name .. struct:GetDeclaration(meta_data) .. ";\n"
+	end
+end
+
+header = header .. extra
+
 local lua = ffibuild.StartLibrary(header)
 
-lua = lua .. "library = " .. meta_data:BuildFunctions("^sf_(.+)")
-lua = lua .. "library.e = " .. meta_data:BuildEnums("^SF_(.+)")
+lua = lua .. "library = " .. meta_data:BuildFunctions("^sf_(.+)", "foo_bar", "FooBar")
+lua = lua .. "library.e = " .. meta_data:BuildEnums("^SF[CMD]?_(.+)")
 
 ffibuild.EndLibrary(lua, header)
