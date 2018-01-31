c482de953132ba21f0f55f71c8a1de20f4ff8e81
CapsAdmin <eliashogstvedt@gmail.com>
2018-01-31 10:37:00 +0100

    use shell scripts to build instead

---
+++ generic.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ "$1" = "clean" ]; then
+	rm -rf repo/
+	mkdir ../tmp
+	mv build.lua ../tmp/
+	mv build.sh ../tmp/
+	mv .gitignore ../tmp/
+	rm -rf ./*
+	mv ../tmp/* .
+	rmdir ../tmp
+	exit
+fi
+
+LUA_DIR=../luajit/
+LUA_BIN="${LUA_DIR}repo/src/luajit"
+echo $LUA_BIN
+
+if [ ! -f $LUA_BIN ]; then
+	pushd $LUA_DIR
+	make
+	popd
+fi
+
+export LD_LIBRARY_PATH="." 
+$LUA_BIN build.lua "$*"
---
+++ opengl/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+source ../generic.sh
