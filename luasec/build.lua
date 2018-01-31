75de26f7b67cc182da5af1360ba9f46f57eb1e9a
CapsAdmin <eliashogstvedt@gmail.com>
2018-01-31 10:16:13 +0100

    separate luasec from luasocket and openssl from luasec

--- luasec/build.lua
+++ luasec/build.lua
@@ -2,6 +2,6 @@ package.path = package.path .. ";../?.lua"
 local ffibuild = require("ffibuild")
 
 ffibuild.NixBuild({
-	package_name = "luajitPackages.luasocket",
-	library_name = "lua/5.1/*"
+	package_name = "luajitPackages.luasec",
+	library_name = "lua/5.1/ssl"
 })
